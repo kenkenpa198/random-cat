fa85271d4b908eb5eb8b0072ffaddaf323ca6a5a
kenkenpa198 <kenkenpa198@gmail.com>
2024-04-13 16:17:50 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,64 @@
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+// getServerSideProps から渡される props の型
+type Props = {
+  initialImageUrl: string;
+};
+
+// ページコンポーネント関数に props を受け取る引数を追加する
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  // 1. useState を使って状態を定義する
+  const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+  const [loading, setLoading] = useState(false); // 初期値は false にしておく
+
+  // 2 マウント時に画像を読み込む宣言
+  // useEffect(() => {
+  //   fetchImage().then((newImage) => {
+  //     setImageUrl(newImage.url); // 画像 URL の状態を更新する
+  //     setLoading(false); // ローディング状態を更新する
+  //   });
+  // }, []);
+
+  // ボタンをクリックしたときに画像を読み込む処理
+  const handleClick = async () => {
+    setLoading(true); // 読込中フラグを立てる
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像 URL の状態を更新する
+    setLoading(false); // 読込中フラグを倒す
+  };
+
+  // 3. ローディング中でなければ画像を表示する
+  return (
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>
+        他のにゃんこも見る
+      </button>
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} />}
+      </div>
+    </div>
+    );
+}
+export default IndexPage;
+
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
+
+type Image = {
+  url: string;
+};
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images);
+  return images[0];
+};
